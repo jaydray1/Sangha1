9b4d63b44d7994bda18e15bd577470a5c097e7ab
JM <jdreher15@gmail.com>
2017-11-21 11:07:16 -0700

    data object

--- components/PrayerStack.js
+++ components/PrayerStack.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import prayerModel from '../models/prayerModel';
-import { SectionList, ScrollView, StyleSheet, Text, Image } from 'react-native';    
+import { SectionList, ScrollView, StyleSheet, Text, Image } from 'react-native';   
 
 export default class PrayerStack extends Component {
     constructor() {
@@ -11,12 +11,22 @@ export default class PrayerStack extends Component {
         }
     }
     render() {
+        const renderedPrayers = prayerModel.map((e, i) => 
+        {
+            return (e)
+        })
         return (
-            <ScrollView style={{height: 200, width: 300}}>
+            <ScrollView style={{width: 350}}>
                 <Image source={prayerModel[0].imagePath}/>
                 <Text>{prayerModel[0].title}</Text>
-                <Text>{prayerModel[0].prayerText}  </Text>
-                <Text>Load</Text>
+                <Text>{prayerModel[0].prayerText}</Text>
+                <Text className="renderings">
+                {
+                    renderedPrayers.map((e, i) => {
+                        return <Text key={i}>{e.title}{e.prayerText}</Text>
+                    })
+                }
+                </Text>
             </ScrollView>
         )
     }
--- models/prayerModel.js
+++ models/prayerModel.js
@@ -1,21 +1,116 @@
 import circleSymbol from '../assets/symbol-cirlce.gif';
+import gassho from '../assets/gassho.jpg';
+import sameWork from '../assets/same-work-buddha.jpg';
+import listeningBuddha from '../assets/listeningBuddha.jpg';
+import invocoAmida from '../assets/invocoAmida.jpg';
+import guanyin from '../assets/guanyin.png';
+import earthLeaf from '../assets/earthLeaf.png';
+import leaf from '../assets/leaf.png';
+import anchor from '../assets/anchor.png';
+import candle from '../assets/candle.png';
+import incense from '../assets/incense.png';
 
 export default [
     {
         title: "Kakuyo Sensei's Open Aspiration",
-        sensei: true,
-        speaker: false,
-        sangha: false,
-        prayerText: "With deepest reverence and compassion, we come together just as we are in the spirit of the Buddha, the Enlightened One, the compassionate. Life is One.We are One with the Buddha.We are One with the Teaching. We are One in the spirit of Universal brotherhood and sisterhood. We resolve to be earnest students of the Buddha and to dedicate our lives to kindness and compassion, following the Way he has pointed out.Embraced by his great compassion may we like him attain the noblest and most peaceful state of Awakening, May we enter the pure land here and now.",
-        imagePath: circleSymbol,
+        prayerText: "Sensei:\n\nWith deepest reverence and compassion, we come together just as we are in the spirit of the Buddha, the Enlightened One, the compassionate. \n\nLife is One. \n\nWe are One with the Buddha. \n\nWe are One with the Teaching. \n\nWe are One in the spirit of Universal brotherhood and sisterhood. \n\nWe resolve to be earnest students of the Buddha and to dedicate our lives to kindness and compassion, following the Way he has pointed out. \n\nEmbraced by his great compassion \n\nmay we like him attain the noblest and most peaceful state of Awakening, \n\nMay we enter the pure land here and now.",
+        imagePath: circleSymbol
 
     }
-    , {
-        prayerText: 'dfkjkas jdf',
-        tile: 'dummy-test',
-        sensei: true,
-        speaker: false,
-        sangha: false,
-        imagePath: null,
+    , 
+    {
+        title: "Sangha Opening",
+        prayerText: "Speaker:\n\nThe Sangha is invited to come back to our breathing so that the collective energy of mindfulness will bring us together as an organism, flowing as a river with no more separation. Let the whole Sangha breathe as one body, chant as one body, listen as one body transcending the boundaries of a delusive self, liberating from the superiority complex, the inferiority complex, and the equality complex\n\nAll:\n\nMy body, speech and mind in perfect oneness. I send my heart along with the sound of the bell. May the hearer awake from forgetfulness and transcend all anxiety and sorrow and hear the name that calls to all of us\n\nNamu Amida Butsu.",
+        imagePath: null
+    }
+    ,
+    {
+        title: "Invocation",
+        prayerText: "Sensei:\n\nWe have gathered together today with open hearted awareness. May we be nourished by each other and by all the Buddhas and Bodhisattvas and our myriad of teachers\n\nin body, \n\nin mind, \n\nin spirit \n\nand in the Way of Oneness..",
+        imagePath: gassho
+    }
+    ,
+    {
+        title: "Invocation",
+        prayerText: "Sensei:\n\nWe have gathered together today with open hearted awareness. May we be nourished by each other and by all the Buddhas and Bodhisattvas and our myriad of teachers\n\nin body, \n\nin mind, \n\nin spirit \n\nand in the Way of Oneness.\n\nSangha\n\nMay our gathering together inspire us \n\nto direct our hearts and minds – \n\nTo heal, not to harm, \n\nTo help, not to hinder, \n\nTo bless, not to curse, \n\nTo always serve the spirit of awakening. \n\nMay It Be So.",
+        imagePath: gassho
+    }
+    ,
+    {
+        title: "Gratitude Affirmation",
+        prayerText: "Sensei\n\n When sorrow invades the mind we find the Buddha’s teachings.Sangha\nThank you sorrow! \n\nSensei\nThrough cruel adversity we discover the Sangha and the transformation of suffering.\n\nSangha\nThank you cruel adversity!\n\nSensei\nWhen foolishness and blind passions overwhelm us we discover the Great compassion of Amida Buddha.\nSangha\nThank you foolishness and passions.\n\nSensei\nThrough the unfolding of the Dharma we place our hands in gassho and bow to the oneness of life.\n\nSangha\nNamu Amida Butsu.",
+        imagePath: null
+    }
+    ,
+    {
+        title: "The Same Work",
+        prayerText: "Sensei\nOur work and the Buddha’s are the same.\nLet us remember that we are called to be compassionate, understanding\nthat…\n\nSangha\nin compassion, there is no respect\nor disrespect,\nnor responsibility, or judgement.\n\nThere is\nno anger,\nno bias,\nno prejudice.\n\nIn compassion, we do not keep others from \nwhat they are doing or what they are thinking,\nwe do not look for cause or blame or give any thought to effort.\n\nIn compassion, our only concern\nis for the suffering\nthat is there.\n\nMay it be so. ",
+        imagePath: sameWork
+    }
+    ,
+    {
+        title: "Deep Listening",
+        prayerText: "Sensei:\nAware of the suffering caused to myself and others by not deeply listening\n…\n\nSangha:\nI vow to cultivate deep listening.\nI vow to listen without judgement and to practice\nnoble silence.\n\nI vow to hear the suffering of others and my own,\nknowing that deep listening is an act of love and compassion.\n\nI vow to cultivate deep listening in order to understand others with wisdom and \ncompassion.\n\nI vow to listen deeply, to cultivate compassion and bring an end to suffering.\n\nI vow to be still and listen for the call of all the Buddhas and of Amitabha to \ncome, to come and dance.\n\nMay it be so. ",
+        imagePath: listeningBuddha
+    }
+    ,
+    {
+        title: "Meta Bhavana",
+        prayerText: "Sensei:\nIn the Metta Sutta the Buddha taught the discourse on Loving Kindness. \nLet us now recite the heart of its teachings. \n\nSangha\nMay every living being, weak or strong, the long and the small, the short \nand the medium-sized, the mean and the great\n\nMay every living being, seen or unseen,\nthose dwelling far off, those nearby, those already born, those waiting to be\nborn\n\nMay all beings everywhere plagued with sufferings of body and mind\nquickly be freed from their illnesses.\n\nMay those frightened cease to be afraid, and may those bound be free. \n\nMay the powerless find power and may people think of befriending one \nanother. \n\nMay those who find themselves in trackless, fearful wildernesses - the children, the aged,\nthe unprotected - be guarded by beneficent bodhisattvas and teachers, \n\nLet no one deceive another\nLet no one despise another in any situation \nLet no one, from resentment, ignorance or hatred, wish evil to anyone at all.\n\nJust as a mother, with her own life, protects her only child from hurt\n\nso within ourselves may we cultivate a boundless concern for all living creatures.\n\nAnd Display a heart of boundless love for all the world…\n\nIn all its height and depth and broad extent\nLove unrestrained, without hate or bitterness\n\nThen as you stand or walk, sit or lie, until overcome by drowsiness devote your mind\n\nentirely to this, this is our vow.\n\nMay all beings be happy.\nMay all beings be happy.\nMay all beings be happy.\n\nAnd free from suffering.\n\nNamu Amida Butsu. ",
+        imagePath: null
+    }
+    ,
+    {
+        title: "Invocation of Amida Buddha",
+        prayerText: "Sensei:\nHearing Namu Amida Butsu, saying\nNamu Amida Butsu awakens a buddha\n\nright here!\n\nThis Buddha; the source of boundless compassion.\n\nThis Buddha gives compassion to all without exception.\n\nJust compassion. Just as you are. \n\nSangha:\nMay each of us cultivate a receiving heart\nand recall this in the light \nand recall this in the dark\n\nMoment after moment the courageous heart arises!\n\nTime after time there is nothing but this\nnothing to accomplish, nothing to do\n\nbut simply “Be”.\n\nNamu-amida butsu.  ",
+        imagePath: invocoAmida
+    }
+    ,
+    {
+        title: "Invocation of Guanyin",
+        prayerText: "Sensei:\nWe call upon Guanyin Bodhisattva of Compassion, mother of all the Buddhas\n\nWe call upon Guanyin to inspire our lives.\nWe call upon ourselves to inspire Guanyin.\nWe call upon ourselves as Guanyin.\n\nEnlightening, being enlightened, calling and responding, the birds and stars as \nGuanyin\n\nThe friend and the enemy as Guanyin.\n\nEach thought, of compassion ever so brief, is\nGuanyin herself turning the Dharma Wheel.\n\nAcceptance is Guanyin;\nRegarding the cries of the world is Guanyin.\n\nGuanyin is realized in hearing the distress and pain of all beings, and embraces \nforests, geckos and children.\n\nSangha:\nMay the compassionate action of Guanyin arise from the place of grateful receiving.\n\nI honor the great power for the Way, generated by the profound act of opening \nmyself to the world. \n\nOm padme hum - namu- ami da bu. ",
+        imagePath: guanyin
+    }
+    ,
+    {
+        title: "Prayer For the Earth",
+        prayerText: "Sensei:\nWe gently caress you, Mother Earth, our planet and our home. \nOur vision has brought us closer to you and to the oneness of all life, making us aware of the harm we have done in our ignorance and greed.\nDaily we are reminded that we have poisoned your waters, your lands, your air.\nWe have taken your boundless grace for granted, Great Earth bodhisattva, for too long.\nAs we awaken to the oneness of life your suffering becomes our suffering.\n\nSangha\nWe gently caress you great earth bodhisattva and we aspire to help heal you, great mother, our planet and our home.\nTouching you gently, we pray that we may become peace-bringers, health bringers and life-bringers so that our home will be a pure land here and now.\n\nNamu Amida Butsu. ",
+        imagePath: earthLeaf
+    }
+    ,
+    {
+        title: "Boundless Light",
+        prayerText: "Sensei:\nMay we awaken to reality as it is.\nMay we learn acceptance as the path to liberation.\nMay we live the questions of our lives and not be attached to the answers.\nMay we embrace the openness of not knowing.\nMay we all enter our awakening and realize the Buddha Way together remembering that…\n\nSangha:\nEven though our eyes may be blinded by confusion, anger and greed and we do not see the brilliant light that embraces us –\n\nThe Great compassion never tires,\nalways casting its boundless light upon us,\n\njust as we are.\n\n\nNamu Amida Butsu",
+        imagePath: null
+    }
+    ,
+    {
+        title: "Letting Go To Freedom",
+        prayerText: "Sensei:\n\nJust as the vast sky does not hinder white clouds...\nJust as the falling maple leaf presents both front and back…\nJust as the water of the lake does not hold the reflection of the bird that flies over…\nJust as the earth accepts all things as they are,\n\nLet us practice letting go…\nLet us be like water without resistance\nLet go of all pretense, scheming and our need to control the uncontrollable.\n\nSangha:\n\nWe vow to practice letting go and walk side by side, supporting one another in our practice, \n\nWe vow to help each other let go of the stories and delusions that have hindered our freedom,\n\nWe vow to find our True selves surrounded and embraced by the oneness of life and by the great compassion of all the Buddhas.\n\nWe vow to cultivate our acceptance of ourselves just as we are. ",
+        imagePath: leaf
+    }
+    ,
+    {
+        title: "Seeking Refuge",
+        prayerText: "Sensei:\nAs we seek refuge in the Buddha, the Dharma and the Sangha, we entrust ourselves to the Oneness of Life.\n\nAs we take refuge in the teachings of the Buddha may we meet our Buddha-nature in our awareness of	the present moment.\n\nAs we seek refuge in the three treasures…\n\nSangha:\nWe vow to live in oneness with the Buddha, the teachings and the Sangha.\n\nOneness in Buddha and in all our teachers, \nOneness in Dharma and in the boundless way,\nOneness in Sangha and in all living beings.  ",
+        imagePath: null
+    }
+    ,
+    {
+        title: "Going For Refuge",
+        prayerText: "Sensei\n\nBuddham Saranam Gacchami\nDhamman Saranam Gacchami\nSangham Saranam Gacchami\n\nSangha\nI go to the Buddha for guidance. I will follow his Way of Oneness. I will awaken to his boundless wisdom and compassion and to my own.\n\nSensei:\nWe take refuge in the Dharma and the Sangha of Boundless light.\n\nSangha:\nI go to the Dharma for guidance. I shall become one with its teachings. By its light and the light of all the Buddhas, I will free myself and all beings from suffering.\n\nI go to the Sangha for guidance. I will become one with the Sangha in a spirit of compassion and harmony to help lead all beings to awakening.\n\nI go for refuge to the Buddha,\nI go for refuge to the Dharma,\nI go for refuge to the Sangha.\n\nWith openness of heart we entrust ourselves to the oneness of life.\nNamu Amida Butsu.",
+        imagePath: anchor
+    }
+    ,
+    {
+        title: "Candle Lighting",
+        prayerText: "Sensei:\nI light this candle to represent the light of Amitabha Buddha.\n\nKO MYO HEN JO\nJI - PPO SE KAI\nNEM BU - SHU JO\nSE - SSHU FU SHA \n\nSangha:\nAs a small light can lead one out of darkness, may this small light be boundless and \nlead others out of the darkness of hatred, greed and confusion. \n\nMay it dispel the darkness of shame\nand suffering, of isolation, and fear. \n\nMay this small flame be a light to each of us -\n\nSo that our light and the light \nof Amitabha Buddha will shine forth\nshowing the way to freedom.\n\nMay it be so.  ",
+        imagePath: candle
+    }
+    ,
+    {
+        title: "Incense Offering",
+        prayerText: "Sensei:\nI light this candle to represent the light of Amitabha Buddha.\n\nKO MYO HEN JO\nJI - PPO SE KAI\nNEM BU - SHU JO\nSE - SSHU FU SHA \n\nSangha:\nAs a small light can lead one out of darkness, may this small light be boundless and \nlead others out of the darkness of hatred, greed and confusion. \n\nMay it dispel the darkness of shame\nand suffering, of isolation, and fear. \n\nMay this small flame be a light to each of us -\n\nSo that our light and the light \nof Amitabha Buddha will shine forth\nshowing the way to freedom.\n\nMay it be so.  ",
+        imagePath: incense
     }
 ]
